80e96dc800c140422fc93b41b7dd5ddec77e4be4
mohsensami <mohsensami18727@gmail.com>
2025-06-07 21:14:37 +0330

    install and config zustand

--- src/providers/Providers.jsx
+++ src/providers/Providers.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
 import { getCookie, setCookie } from "../utils/helpers/cookie";
+import useStore from "../store";
 
-const Authorize = () => {
+const Authorize = ({ children }) => {
+  const { setState } = useStore();
   useEffect(() => {
     const readCookie = async () => {
       const result = await getCookie("credential");
+      setState(result);
       console.log(result);
     };
 
     readCookie();
   }, []);
+  return <>{children}</>;
 };
 
 const Providers = ({ children }) => {
