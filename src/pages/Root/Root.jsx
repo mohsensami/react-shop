411965dbb81ace9cb306e449af8585e9c6062891
mohsensami <mohsensami18727@gmail.com>
2025-06-11 22:27:24 +0330

    ProductsGridWithPagination and getProductsApi.js added to Root page

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../components/common/Header/Header";
 import CategoriesChips from "../../components/common/CategoriesChips";
+import ProductsGridWithPagination from "../../components/common/ProductsGridWithPagination";
 
 const Root = () => {
   return (
     <div>
       <Header />
       <CategoriesChips />
+      <ProductsGridWithPagination />
     </div>
   );
 };
